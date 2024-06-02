c1267540814a2b6b427f766dbfbbc032c10f408c
Anschel Burk <anschel.burk@gmail.com>
2024-06-02 17:52:11 -0400

    Modified the following to configure backend of FullCalendar: mysite.urls, planner.models, planner.views, planner.urls.

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    
+    def __str__(self):
+        return f'{self.title}, {self.start} - {self.end}'
--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,23 @@
+from .models import Event
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+def calendar_view(request):
+    return render(request, 'calendar.html')
+
+@csrf_exempt
+def create_event(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        event = Event.objects.create(
+            title=data['title'],
+            start=data['start'],
+            end=data['end']
+        )
+        return JsonResponse({'id': event.id})
+    
+def get_events(request):
+    events = Event.objects.all().values('id', 'title', 'start', 'end')
+    return JsonResponse(list(events), safe=False)
