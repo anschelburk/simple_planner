347b2d06866b232018a70f46d96f50aca26c15a1
Anschel Burk <anschel.burk@gmail.com>
2024-06-06 12:24:22 -0400

    Committing code so I can merge PR #14.

--- mysite/planner/urls.py
+++ mysite/planner/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('api/create_event/', views.create_event, name='create_event'),
     path('api/get_events/', views.get_events, name='get_events'),
-    path('api/update_event', views.create_event, name='update_event'),
+    path('api/update_event', views.update_event, name='update_event'),
     path('calendar/', views.calendar_view, name='calendar'),
 ]
